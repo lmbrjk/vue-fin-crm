465d3124a89328123416419d731b6dacb9ad9d05
Evgeny <lmbrjk22ak@gmail.com>
2020-12-11 16:24:40 +0700

    main layout

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul
+      v-bind:class="{open: value}" 
+      class="sidenav app-sidenav"
+    >
+      <router-link
+        v-for="link in links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        v-bind:to="link.url"
+        v-bind:exact="link.exact"
+      >
+        <a class="waves-effect waves-orange pointer">{{ link.title }}</a>
+      </router-link>
+    </ul>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data(){
+    return {
+      links: [
+        {title: "Счет", url: "/", exact: true},
+        {title: "История", url: "/history"},
+        {title: "Планирование", url: "/planning"},
+        {title: "Новая запись", url: "/record"},
+        {title: "Категории", url: "/categories"}
+      ]
+    }
+  }
+}
+</script>
