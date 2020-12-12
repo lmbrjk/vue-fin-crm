f380acf5bdb34a77b4b938080af5567bccb8b9d3
lmbrjk <lmbrjk22ak@gmail.com>
2020-12-13 01:15:38 +0700

    date, logout

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -7,16 +7,17 @@
           >
             <i class="material-icons black-text">dehaze</i>
           </a>
-          <span class="black-text">12.12.12</span>
+
+          <span class="black-text">{{ date | date("datetime") }}</span>
+
         </div>
 
         <ul class="right hide-on-small-and-down">
           <li>
             <a
-                class="dropdown-trigger black-text"
-                href="#"
-                data-target="dropdown"
-                ref="dropdown"
+              class="dropdown-trigger black-text"
+              data-target="dropdown"
+              ref="dropdown"
             >
               USER NAME
               <i class="material-icons right">arrow_drop_down</i>
@@ -51,13 +52,40 @@
 import M from 'materialize-css'
 
 export default {
+  data(){
+    return {
+      // текущая дата
+      date: new Date(),
+      // интервал для постоянного отсчета текущего времени
+      interval: null,
+      // выпадающее меню при нажатии на пользователя
+      dropdown: null
+    }
+  },
   methods: {
+    // выход пользователя
     logout(){
       this.$router.push("/login?message=logout");
     }
   },
   mounted(){
-    M.Dropdown.init(this.$refs.dropdown, {constrainWidth: true});
+    // вывод текущего времени в навбаре
+    this.interval = setInterval( () => {
+      this.date = Date.now();
+    }, 1000);
+    // подключение выпадающего меню к кнопке логина
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {constrainWidth: true});
+  },
+  beforeDestroy(){
+    // удаление вывода текущего времени в навбаре если навбара нет на текущей странице
+    clearInterval(this.interval);
+
+    // удаление выпадающего меню в навбаре если навбара нет на текущей странице
+    // используем проверку что если все подключено, то удаляем
+    if(this.dropdown && this.dropdown.destroy){
+      this.dropdown.destroy();
+    }
+
   }
 }
 </script>
