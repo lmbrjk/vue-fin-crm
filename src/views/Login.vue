628d8641ea9206f76ebfc7bb4675831eb8eec6dc
lmbrjk <lmbrjk22ak@gmail.com>
2020-12-14 23:10:57 +0700

    5 vuelidate for login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,61 +7,61 @@
         <div class="card-content">
             <span class="card-title">Домашняя бухгалтерия</span>
             <div class="input-field">
-            <input
-                v-model.trim="email"
-                v-bind:class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && $v.email.email)}"
+                <input
+                    v-model.trim="email"
+                    v-bind:class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
 
-                id="email"
-                type="text"
-                class="validate"
-            >
-            <label for="email">Email</label>
-            <small
-                v-if="$v.email.$dirty && !$v.email.required"
-                class="helper-text invalid"
-            >
-                Поле Email не должно быть пустым
-            </small>
-            <small
-                v-else-if="$v.email.$dirty && $v.email.email"
-                class="helper-text invalid"
-            >
-                Введите корректный Email
-            </small>
+                    id="email"
+                    type="text"
+                    class="validate"
+                >
+                <label for="email">Email</label>
+                <small
+                    v-if="$v.email.$dirty && !$v.email.required"
+                    class="helper-text invalid"
+                >
+                    Поле Email не должно быть пустым
+                </small>
+                <small
+                    v-else-if="$v.email.$dirty && !$v.email.email"
+                    class="helper-text invalid"
+                >
+                    Введите корректный Email
+                </small>
             </div>
             <div class="input-field">
-            <input
-                v-model.trim="password"
-                v-bind:class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && $v.password.minLength)}"
+                <input
+                    v-model.trim="password"
+                    v-bind:class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
 
-                id="password"
-                type="password"
-                class="validate"
-            >
-            <label for="password">Пароль</label>
-            <small
-                v-if="$v.password.$dirty && !$v.password.required"
-                class="helper-text invalid"
-            >
-                Поле не должно быть пустым
-            </small>
-            <small
-                v-else-if="$v.password.$dirty && $v.password.minLength"
-                class="helper-text invalid"
-            >
-                Пароль должен быть не менее {{ $v.password.$params.minLength.min }} символов
-            </small>
+                    id="password"
+                    type="password"
+                    class="validate"
+                >
+                <label for="password">Пароль</label>
+                <small
+                    v-if="$v.password.$dirty && !$v.password.required"
+                    class="helper-text invalid"
+                >
+                    Поле не должно быть пустым
+                </small>
+                <small
+                    v-else-if="$v.password.$dirty && !$v.password.minLength"
+                    class="helper-text invalid"
+                >
+                    Пароль должен быть не менее {{ $v.password.$params.minLength.min }} символов
+                </small>
             </div>
         </div>
         <div class="card-action">
             <div>
-            <button
-                class="btn waves-effect waves-light auth-submit"
-                type="submit"
-            >
-                Войти
-                <i class="material-icons right">send</i>
-            </button>
+                <button
+                    class="btn waves-effect waves-light auth-submit"
+                    type="submit"
+                >
+                    Войти
+                    <i class="material-icons right">send</i>
+                </button>
             </div>
 
             <p class="center">
@@ -96,6 +96,12 @@ export default {
                 this.$v.$touch();
                 return
             }
+
+            const formData = {
+                email: this.email,
+                password: this.password
+            };
+            console.log(formData);
 
             this.$router.push('/');
         }
