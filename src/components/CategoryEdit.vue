46da891347bfd74f569ee78fe34251d834c31241
lmbrjk <lmbrjk22ak@gmail.com>
2020-12-22 00:07:50 +0700

    11 70%

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -8,37 +8,63 @@
             <form>
                 <div class="input-field" >
                 <select
+                    v-model="current"
                     ref="select"
                 >
                     <option
-                        v-for="cat of categories"
-                        v-bind:key="cat.id"
-                        v-bind:value="cat.id"
+                        v-for="category of categories"
+                        v-bind:key="category.id"
+                        v-bind:value="category.id"
                     >
-                        {{ cat.categoryName }}
+                        {{ category.categoryName }}
                     </option>
                 </select>
                 <label>Выберите категорию</label>
                 </div>
 
                 <div class="input-field">
-                <input type="text" id="name">
-                <label for="name">Название</label>
-                <span class="helper-text invalid">TITLE</span>
+                    <input
+                        v-model="categoryName"
+                        v-bind:class="{invalid: $v.categoryName.$dirty && !$v.categoryName.required}"
+
+                        id="name"
+                        type="text"
+                    >
+                    <label for="name">Название</label>
+                    <span
+                        v-if="$v.categoryName.$dirty && !$v.categoryName.required"
+                        class="helper-text invalid"
+                    >
+                        Введите имя категории
+                    </span>
                 </div>
 
                 <div class="input-field">
-                <input
-                    id="limit"
-                    type="number"
-                >
-                <label for="limit">Лимит</label>
-                <span class="helper-text invalid">LIMIT</span>
+                    <input
+                        v-model.number.trim="limit"
+                        v-bind:class="{invalid: ($v.limit.$dirty && !$v.limit.required) || ($v.limit.$dirty && !$v.limit.minValue)}"
+
+                        id="limit"
+                        type="number"
+                    >
+                    <label for="limit">Лимит</label>
+                    <span
+                        v-if="$v.limit.$dirty && !$v.limit.minValue "
+                        class="helper-text invalid"
+                    >
+                        Минимальное значение {{ $v.limit.$params.minValue.min }}
+                    </span>
+                    <span
+                        v-else-if="$v.limit.$dirty && !$v.limit.required "
+                        class="helper-text invalid"
+                    >
+                        Поле не должно быть пустым
+                    </span>
                 </div>
 
                 <button class="btn waves-effect waves-light" type="submit">
-                Обновить
-                <i class="material-icons right">send</i>
+                    Обновить
+                    <i class="material-icons right">send</i>
                 </button>
             </form>
         </div>
@@ -46,15 +72,44 @@
 </template>
 
 <script>
+import {required, minValue} from "vuelidate/lib/validators"
+
 export default {
     props: ["categories"],
     data(){
         return {
-            select: null
+            select: null,
+            categoryName: '',
+            limit: 100,
+            current: null
         }
     },
+    validations: {
+        categoryName: {required},
+        limit: {required, minValue: minValue(100)}
+    },
+    watch: {
+        // при выборе в селекте категории будут показаны данные этой катгории
+        current(catId){
+            const {categoryName, limit} = this.categories.find(c => c.id == catId);
+
+            this.categoryName = categoryName;
+            this.limit = limit;
+        }
+    },
+    created(){
+        const {id, categoryName, limit} = this.categories[0];
+
+        this.current = id;
+        this.categoryName = categoryName;
+        this.limit = limit;
+    },
     mounted(){
+        // привязка к селекту materialize
         this.select = window.M.FormSelect.init(this.$refs.select);
+        
+        window.M.updateTextFields();
+
     },
     destroyed(){
         if(this.select && this.select.destroy){
--- src/store/category.js
+++ src/store/category.js
@@ -36,6 +36,7 @@ export default {
                 commit("setError", e);
                 throw e;
             }
-        }
+        }        
+
     }
 }
