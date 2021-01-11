a18831f4ec82dd3c5d8b34c25f31e6e15dd07eac
Evgeny <lmbrjk22ak@gmail.com>
2021-01-11 17:19:15 +0700

    19 30%

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,16 +4,31 @@
     <h3>Профиль</h3>
   </div>
 
-  <form class="form">
+  <form 
+    v-on:submit.prevent="submitHandler"
+    class="form"
+  >
     <div class="input-field">
       <input 
         v-model="name"
+        v-bind:class="{invalid: ($v.name.$dirty && !$v.name.required) || ($v.name.$dirty && !$v.name.minLength)}"
 
         id="description"
         type="text"
       >
       <label for="description">Имя</label>
-      <span class="helper-text invalid">name</span>
+      <small
+        v-if="$v.name.$dirty && !$v.name.required"
+        class="helper-text invalid"
+      >
+        Поле не должно быть пустым
+      </small>
+      <small
+        v-else-if="$v.name.$dirty && !$v.name.minLength"
+        class="helper-text invalid"
+      >
+        Имя должно быть не менее {{ $v.name.$params.minLength.min }} символов
+      </small>
     </div>
 
     <div class="switch">
@@ -35,6 +50,8 @@
 
 <script>
 import {mapGetters} from "vuex"
+import {required, minLength} from "vuelidate/lib/validators"
+import messages from "@/utils/messages"
 
 export default {
   data(){
@@ -42,8 +59,24 @@ export default {
       name: ""
     }
   },
+  validations: {
+    name: {required, minLength: minLength(4)}
+  },
   computed: {
     ...mapGetters(["info"]) 
+  },
+  methods: {
+    async submitHandler(){
+      if(this.$v.$invalid){
+        this.$v.$touch();
+        return
+      }
+
+      try {
+        await this.$store.dispatch("updateInfo", {name: this.name});
+      } catch (e){}
+
+    }
   },
   async mounted(){
     this.name = this.info.name;
